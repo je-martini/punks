0e8e5be2d695e5a44a2e884e756f834cb0bf9c4b
je-martini <jesusmartini2017@gmail.com>
2023-07-23 08:46:08 -0400

    first deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks:{  
+    sepolia:{
+      url: `https://sepolia.infura.io/v3/4e7b5be588264b51909c59ab417f3d09`,
+      accounts: [
+        "1c04dc5ef858bad4f49b7bf6683de4d3b43c5ca75eb8b28a7aa6dc266c0b50c0",
+        
+      ],
+    },
+  }, 
 };
