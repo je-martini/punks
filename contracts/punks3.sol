06b1b5dd29c05110b89e0c1d113aa986a6ce3818
je-martini <jesusmartini2017@gmail.com>
2023-07-27 20:14:12 -0400

    finishing the set up for the image metadata

--- contracts/punks3.sol
+++ contracts/punks3.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.2 ;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";    
+import "@openzeppelin/contracts/utils/Counters.sol";   
+import "./base64.sol"; 
 
 contract punks3 is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
@@ -29,9 +30,17 @@ contract punks3 is ERC721, ERC721Enumerable {
         returns( string memory){
             require(_exists(token_Id), "ERC721 Metadata: URI query for nonexistent token");
         
-        string memory json_URI;
+        string memory json_URI = base64.encode(
+            abi.encodePacked(
+                '{ "name": "punks #',
+                token_Id,
+                '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
+                "// TO DO Calculate image URL",
+                '"}' 
+            ),
+        );
         
-        return "";
+        return string(abi.encodePacked("data:application/json;base64,", json_URI));
 
         }
 
